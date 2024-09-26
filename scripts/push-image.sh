7fcb9e8b231ef4a3264d259655d8fe615770c0e4
ritamrao <54271550+ritamrao@users.noreply.github.com>
2024-09-27 00:37:19 +1000

    Update push-image.sh

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -17,5 +17,5 @@ set -u # or set -o nounset
 : "$VERSION"
 : "$REGISTRY_UN"
 : "$REGISTRY_PW"
-echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
+echo $REGISTRY_PW| docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
 docker push $CONTAINER_REGISTRY/video-streaming:$VERSION
